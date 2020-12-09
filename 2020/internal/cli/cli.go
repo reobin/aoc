1647ba90295252f319bd89e0e1534cb98cf1f636
Robin Gagnon <me@reobin.dev>
2020-12-08 20:10:44 -0500

    2020: Add benchmark command

---
+++ 2020/internal/cli/cli.go
@@ -0,0 +1,72 @@
+package cli
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+
+	"github.com/reobin/aoc/2020/day"
+)
+
+const minDayNumber = 1
+const maxDayNumber = 25
+
+var DayRunnerMap = map[int]interface{}{
+	1: day.RunDay01,
+	2: day.RunDay02,
+	3: day.RunDay03,
+	4: day.RunDay04,
+	5: day.RunDay05,
+	6: day.RunDay06,
+	7: day.RunDay07,
+	8: day.RunDay08,
+}
+
+// GetDayArg gets day number from os arguments
+func GetDayArg(osArgs []string) (int, error) {
+	if len(osArgs) > 1 {
+		value, err := getDayValue(osArgs[1])
+		if err != nil {
+			return 0, err
+		}
+		return value, nil
+	}
+
+	return 0, errors.New("Please specify a day to run")
+}
+
+func getDayValue(dayArg string) (int, error) {
+	day, err := strconv.Atoi(dayArg)
+	if err != nil {
+		return 0, errors.New("Please provide a valid day number")
+	}
+
+	if day < minDayNumber || day > maxDayNumber {
+		return 0, fmt.Errorf("Please provide a day number between %d and %d", minDayNumber, maxDayNumber)
+	}
+
+	return day, nil
+}
+
+func GetDayInput(day int, fileNamePrefix string) (string, error) {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	dayKey := strconv.Itoa(day)
+	if day < 10 {
+		dayKey = fmt.Sprintf("0%s", dayKey)
+	}
+
+	inputPath := fmt.Sprintf("%s/input/%s%s.txt", workingDirectory, fileNamePrefix, dayKey)
+
+	input, err := ioutil.ReadFile(inputPath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(input), nil
+}
--- 2020/internal/cli/cli_test.go
+++ 2020/internal/cli/cli_test.go
@@ -1,4 +1,4 @@
-package main
+package cli
 
 import (
 	"fmt"
@@ -12,26 +12,26 @@ import (
 func TestGetDayArg(t *testing.T) {
 	t.Run("should return second argument", func(t *testing.T) {
 		osArgs := []string{"function", "01"}
-		dayArg, err := getDayArg(osArgs)
+		dayArg, err := GetDayArg(osArgs)
 		if err != nil {
-			t.Errorf("Incorrect result for getDayArg; got error: %s", err)
+			t.Errorf("Incorrect result for GetDayArg; got error: %s", err)
 		}
 
-		if dayArg != "01" {
-			t.Errorf("Incorrect result for getDayArg; got: %s, want: %s", dayArg, "01")
+		if dayArg != 1 {
+			t.Errorf("Incorrect result for GetDayArg; got: %d, want: %s", dayArg, "01")
 		}
 	})
 
 	t.Run("should return error if second argument is missing", func(t *testing.T) {
 		osArgs := []string{"function"}
-		dayArg, err := getDayArg(osArgs)
+		dayArg, err := GetDayArg(osArgs)
 
 		if err == nil {
 			t.Error("Incorrect result for getDayArg; got no error")
 		}
 
-		if dayArg != "" {
-			t.Errorf("Incorrect result for getDayArg; got: %s, want: %s", dayArg, "")
+		if dayArg != 0 {
+			t.Errorf("Incorrect result for GetDayArg; got: %d, want: %s", dayArg, "")
 		}
 	})
 }
@@ -56,8 +56,8 @@ func TestGetDayValue(t *testing.T) {
 			t.Error("Incorrect result for getDayValue; got no error")
 		}
 
-		if day != -1 {
-			t.Errorf("Incorrect result for getDayValue; got: %d, want: %d", day, -1)
+		if day != 0 {
+			t.Errorf("Incorrect result for getDayValue; got: %d, want: %d", day, 0)
 		}
 	})
 
@@ -68,8 +68,8 @@ func TestGetDayValue(t *testing.T) {
 			t.Error("Incorrect result for getDayValue; got no error")
 		}
 
-		if day != -1 {
-			t.Errorf("Incorrect result for getDayValue; got: %d, want: %d", day, -1)
+		if day != 0 {
+			t.Errorf("Incorrect result for getDayValue; got: %d, want: %d", day, 0)
 		}
 	})
 }
@@ -101,7 +101,7 @@ func TestGetDayInput(t *testing.T) {
 
 		workingDirectory, err := os.Getwd()
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, error getting wd: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, error getting wd: %s", err)
 		}
 
 		target := fmt.Sprintf("%s/input/.tmp.10.txt", workingDirectory)
@@ -112,25 +112,25 @@ func TestGetDayInput(t *testing.T) {
 
 		file, err := os.Create(target)
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, error creating file: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, error creating file: %s", err)
 		}
 
 		if _, err := os.Stat(target); os.IsNotExist(err) {
-			t.Error("Incorrect result for getDayInput, did not create file")
+			t.Error("Incorrect result for GetDayInput, did not create file")
 		}
 
 		_, err = file.WriteString(expectedFileContent)
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, error writing to file: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, error writing to file: %s", err)
 		}
 
-		input, err := getDayInput(10, ".tmp.")
+		input, err := GetDayInput(10, ".tmp.")
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, got error: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, got error: %s", err)
 		}
 
 		if input != expectedFileContent {
-			t.Errorf("Incorrect result for getDayInput, got: %s, want: %s", input, expectedFileContent)
+			t.Errorf("Incorrect result for GetDayInput, got: %s, want: %s", input, expectedFileContent)
 		}
 
 		cleanUp(target)
@@ -143,7 +143,7 @@ func TestGetDayInput(t *testing.T) {
 
 		workingDirectory, err := os.Getwd()
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, error getting wd: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, error getting wd: %s", err)
 		}
 
 		target := fmt.Sprintf("%s/input/.tmp.01.txt", workingDirectory)
@@ -154,25 +154,25 @@ func TestGetDayInput(t *testing.T) {
 
 		file, err := os.Create(target)
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, error creating file: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, error creating file: %s", err)
 		}
 
 		if _, err := os.Stat(target); os.IsNotExist(err) {
-			t.Error("Incorrect result for getDayInput, did not create file")
+			t.Error("Incorrect result for GetDayInput, did not create file")
 		}
 
 		_, err = file.WriteString(expectedFileContent)
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, error writing to file: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, error writing to file: %s", err)
 		}
 
-		input, err := getDayInput(1, ".tmp.")
+		input, err := GetDayInput(1, ".tmp.")
 		if err != nil {
-			t.Errorf("Incorrect result for getDayInput, got error: %s", err)
+			t.Errorf("Incorrect result for GetDayInput, got error: %s", err)
 		}
 
 		if input != expectedFileContent {
-			t.Errorf("Incorrect result for getDayInput, got: %s, want: %s", input, expectedFileContent)
+			t.Errorf("Incorrect result for GetDayInput, got: %s, want: %s", input, expectedFileContent)
 		}
 
 		cleanUp(target)
@@ -183,10 +183,10 @@ func TestGetDayInput(t *testing.T) {
 	t.Run("should return error if day input does not exist", func(t *testing.T) {
 		setWdToRoot()
 
-		_, err := getDayInput(1, ".tmp.")
+		_, err := GetDayInput(1, ".tmp.")
 
 		if err == nil {
-			t.Error("Incorrect result for getDayInput, did not get error")
+			t.Error("Incorrect result for GetDayInput, did not get error")
 		}
 
 		resetWd()
