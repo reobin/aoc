df86e037e7cc38ad02569572d990fedfa691efd1
Robin Gagnon <me@reobin.dev>
2020-12-26 08:42:44 -0500

    2019: Complete day 01 challenge

--- 2019/test/01_test.exs
+++ 2019/test/01_test.exs
@@ -3,12 +3,16 @@ defmodule AoC.Day01Test do
   doctest AoC.Day01
 
   test "part 1" do
-    input = "test input"
-    assert AoC.Day01.part_1(input) == "part 1"
+    input = "100756
+1969"
+
+    assert AoC.Day01.part_1(input) == 33583 + 654
   end
 
   test "part 2" do
-    input = "test input"
-    assert AoC.Day01.part_2(input) == "part 2"
+    input = "1969
+100756"
+
+    assert AoC.Day01.part_2(input) == 966 + 50346
   end
 end
