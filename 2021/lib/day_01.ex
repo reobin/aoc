47dfde5bc4677f1c631996938bb80ef7c09a1c88
Robin Gagnon <me@reobin.dev>
2021-12-01 07:42:37 -0500

    2021: Complete day 01 challenge

--- 2021/lib/day_01.ex
+++ 2021/lib/day_01.ex
@@ -1,11 +1,25 @@
 defmodule AoC.Day01 do
   def part_1(input) do
-    IO.inspect(input)
-    "part 1"
+    input
+    |> get_measurements()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(&did_increase?/1)
   end
 
   def part_2(input) do
-    IO.inspect(input)
-    "part 2"
+    input
+    |> get_measurements()
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(&did_increase?/1)
   end
+
+  defp get_measurements(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp did_increase?([a, b]), do: b > a
 end
--- 2021/test/day_01_test.exs
+++ 2021/test/day_01_test.exs
@@ -5,16 +5,36 @@ defmodule AoC.Day01Test do
   alias AoC.Day01
 
   describe "part 1" do
-    test "part 1" do
-      input = "test input"
-      assert Day01.part_1(input) == "part 1"
+    test "sample 1" do
+      input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+
+      assert Day01.part_1(input) == 7
     end
   end
 
   describe "part 2" do
-    test "part 2" do
-      input = "test input"
-      assert Day01.part_2(input) == "part 2"
+    test "sample 1" do
+      input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+
+      assert Day01.part_2(input) == 5
     end
   end
 end
